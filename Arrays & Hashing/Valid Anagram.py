615e1c10af27d8748083ad0e0088a2dbbafb674c
zakeer-official <zzakeer22@gmail.com>
2024-07-16 16:58:17 +0530

    commit

---
+++ Arrays & Hashing/Valid Anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(t) == len(s):
+            for i in s:
+                if i in t:
+                    t = t.replace(i,"",1)
+        else:
+            return False
+        return len(t) == 0
+
+
+#Example
+
+s = "racecar"
+t = "carrace"
+
+x = Solution()
+print(x.isAnagram(s,t))
