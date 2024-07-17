11a4a12773c8692310477a6b0e79846c55f4da7a
zakeer-official <zzakeer22@gmail.com>
2024-07-17 16:30:47 +0530

    commit

---
+++ Stack/Min Stack.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+        
+
+#Example
+
+Input: ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
+
+Output: [null,null,null,null,0,null,2,1]
+
+Explanation:
+minStack = new MinStack();
+minStack.push(1);
+minStack.push(2);
+minStack.push(0);
+minStack.getMin(); // return 0
+minStack.pop();
+minStack.top();    // return 2
+minStack.getMin(); // return 1
