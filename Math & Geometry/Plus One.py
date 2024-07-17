088ef6f3bb4f31274a24453aa7340df8f6137adf
zakeer-official <zzakeer22@gmail.com>
2024-07-17 16:58:01 +0530

    update

--- Math & Geometry/Plus One.py
+++ Math & Geometry/Plus One.py
@@ -2,8 +2,7 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        x = int("".join(map(str,digits)))
-        x = x + 1
+        x = int("".join(map(str,digits))) + 1
         return [int(i) for i in str(x)]
 
 
