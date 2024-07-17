fc3ed7b2f3ee375432337baa4346ac9bf58321fe
zakeer-official <zzakeer22@gmail.com>
2024-07-17 16:55:30 +0530

    commit

---
+++ Math & Geometry/Plus One.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        x = int("".join(map(str,digits)))
+        x = x + 1
+        return [int(i) for i in str(x)]
+
+
+#Example
+
+digits = [1,2,3,4]
+a = Solution()
+print(a.plusOne(digits))
