db56388270d119f893fdb1b5415e286e5f17fc21
zakeer-official <zzakeer22@gmail.com>
2024-07-17 17:28:04 +0530

    commit

---
+++ Math & Geometry/Pow(x,n).py
@@ -0,0 +1,22 @@
+import math
+
+'''
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+'''
+
+'''
+               (or)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return pow(x,n)
+'''
+
+#Example
+
+x = 2.00000
+n = 5
+
+a = Solution()
+print(x.myPow(x,n))
